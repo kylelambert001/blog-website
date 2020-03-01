4ffd66a6cbe4a2452af39f9402c6c6c3527452b8
kylelambert001 <kylelambert001@gmail.com>
2020-03-01 22:51:26 +0900

    commit

--- server.js
+++ server.js
@@ -1,17 +1,64 @@
 const express = require('express');
 const app = express();
 
+const posts = [{
+    title: 'Post 1',
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.'
+  },
+  {
+    title: 'Post 1',
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.'
+  },
+  {
+    title: 'Post 1',
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.'
+  }
+];
+
 const homeContent = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
 
 const aboutContent = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.';
 
+const contactContent = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.';
+
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.get('/', (req, res) => {
   res.render('home', {
-    content: homeContent
+    content: homeContent,
+    posts: posts
   });
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    content: aboutContent
+  });
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {
+    content: contactContent
+  });
+})
+
+app.get('/compose', (req, res) => {
+  res.render('compose');
+})
+
+app.post('/compose', (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  }
+
+  posts.push(post);
+  res.redirect('/');
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'));
